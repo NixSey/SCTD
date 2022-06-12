b06082cb40b3ee4957703c7d374359865374d4d2
NixSey <106969535+NixSey@users.noreply.github.com>
2022-06-12 20:20:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 # Some Ideas from: KIPAS, Geruays & Revan AR
 # Script created by PhynX
 
-import requests, threading, datetime, sys, os
+import requests, threading, datetime, sys, os, time
 
 os.system('cls' if os.name == 'nt' else 'clear')
 print(f"\t   Simple Crown/Trophy Duplicators for Stumble Guys")
 print(f"\t\t\tBy: KIPASGTS & PhynX")
 print("="*64)
+maxerr = 1000 # Avoid Ban when user AFK.
 api = "kitkabackend.eastus.cloudapp.azure.com:5010"
 auth = str(input("Auth Key: "))
 pos = int(input("""0 = Round 1 (Eliminated)
@@ -15,7 +16,9 @@ pos = int(input("""0 = Round 1 (Eliminated)
 3 = Round 3 (Winner)
 Note: Please input Correctly to Avoid Ban.
 Input: """))
+del = float(input("Delay per Requests (Ex. 0.5, 1.0, 1.5, and etc): "))
 thr = int(input("Threads: "))
+print("="*64)
 
 def s():
         while True:
@@ -33,10 +36,13 @@ def s():
                         if response.status_code == 200:
                                 trof = response.text.split('"SkillRating":')[1].split(',')[0]
                                 cro = response.text.split('"Crowns":')[1].split(',')[0]
-                                sys.stdout.write(f"\r[{dt.year}-{dt.month}-{dt.day} {dt.hour}:{dt.minute}:{dt.second}] SUCCESS!! | Trophy: {trof} | Crowns: {cro}")
+                                sys.stdout.write(f"\r[{dt.year}-{dt.month}-{dt.day} {dt.hour}:{dt.minute}:{dt.second}] Success | Trophy: {trof} | Crowns: {cro}")
                                 sys.stdout.flush()
                         else:
+                                maxerr = maxerr + 1
                                 print(f"[{response.status_code}] Failed")
+                                if (maxerr >= 1000) break # Avoid Ban Detection
+                        time.sleep(del)
                 except Exception as e:
                         print(e)
                         sys.exit(0)
